8035f7354df4ceea72d2a3f7f0a11c7913d1723c
adityajain256 <jainaditya25522@gmail.com>
2026-04-18 00:00:52 +0530

    whatsapp api

---
+++ backend/src/config/whatsapp.ts
@@ -0,0 +1,111 @@
+export const sendWhatsAppMessage = async (
+  to: string,
+  message: string,
+  lang: string,
+) => {
+  const apiKey = process.env.WHATSAPP_API_KEY;
+  const senderId = process.env.WHATSAPP_SENDER_ID;
+  const version = process.env.WHATSAPP_API_VERSION || "v20.0"; // Facebook Graph API version
+  if (!apiKey || !senderId) {
+    console.error("WhatsApp API key or sender ID is not set.");
+    return;
+  }
+  const url = `https://graph.facebook.com/${version}/${senderId}/messages`;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: to,
+        type: "template",
+        template: {
+          name: message,
+          language: {
+            code: lang,
+          },
+        },
+      }),
+    });
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
+};
+
+export const sendDueWhatsappMessage = async (
+  to: string,
+  message: string,
+  lang: string,
+  dueAmount: number,
+  quantity: number,
+  dueDate: string,
+  petrolPumpName: string,
+) => {
+  const apiKey = process.env.WHATSAPP_API_KEY;
+  const senderId = process.env.WHATSAPP_SENDER_ID;
+  const version = process.env.WHATSAPP_API_VERSION || "v20.0"; // Facebook Graph API version
+  if (!apiKey || !senderId) {
+    console.error("WhatsApp API key or sender ID is not set.");
+    return;
+  }
+  const url = `https://graph.facebook.com/${version}/${senderId}/messages`;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: to,
+        type: "template",
+        template: {
+          name: message,
+          language: {
+            code: lang,
+          },
+          components: [
+            {
+              type: "header",
+              parameters: [
+                {
+                  type: "text",
+                  text: "Payment Due Reminder",
+                },
+                {
+                  type: "text",
+                  text: petrolPumpName,
+                },
+              ],
+            },
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: String(dueAmount),
+                },
+                {
+                  type: "text",
+                  text: String(quantity),
+                },
+                {
+                  type: "text",
+                  text: dueDate.slice(0, 10),
+                },
+              ],
+            },
+          ],
+        },
+      }),
+    });
+    return await response.json();
+  } catch (error) {
+    return error;
+  }
+};
