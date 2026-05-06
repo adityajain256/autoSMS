b8ee610477792df0ca0118a0f915ae129ace08c8
adityajain256 <jainaditya25522@gmail.com>
2026-05-06 15:17:24 +0530

    mail api update

--- backend/src/config/mail.ts
+++ backend/src/config/mail.ts
@@ -1,35 +1,71 @@
-import nodemailer from "nodemailer";
 import logger from "../utils/logger.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// export const sendMail = async (
+//   to: string,
+//   subject: string,
+//   template: string,
+// ) => {
+//   const transportOptions = {
+//     host: process.env.SMTP_HOST!,
+//     port: Number(process.env.SMTP_PORT),
+//     secure: false,
+//     auth: {
+//       user: process.env.SMTP_USER!,
+//       pass: process.env.SMTP_PASS!,
+//     },
+//     connectionTimeout: 10000,
+//     greetingTimeout: 10000,
+//     socketTimeout: 15000,
+//   };
+//   console.log("SMTP CONFIG:", {
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT),
+//     user: process.env.SMTP_USER,
+//     passSet: !!process.env.SMTP_PASS, // don't log the actual key
+//   });
+//   const transporter = nodemailer.createTransport(transportOptions);
+//   try {
+//     await transporter.sendMail({
+//       from: process.env.SMTP_USER,
+//       to: to,
+//       subject: subject,
+//       html: template,
+//     });
+
+//     logger.info(`mail sent to ${to}`);
+//   } catch (error) {
+//     logger.error(error);
+//   }
+// };
 
 export const sendMail = async (
   to: string,
   subject: string,
   template: string,
 ) => {
-  const transportOptions = {
-    host: process.env.SMTP_HOST!,
-    port: Number(process.env.SMTP_PORT),
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER!,
-      pass: process.env.SMTP_PASS!,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 15000,
-  };
-
-  const transporter = nodemailer.createTransport(transportOptions);
   try {
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: to,
-      subject: subject,
-      html: template,
+    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "api-key": process.env.BREVO_API_KEY!,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sender: { email: process.env.SMTP_USER },
+        to: [{ email: to }],
+        subject,
+        htmlContent: template,
+      }),
     });
+
+    if (!response.ok) {
+      const err = await response.json();
+      logger.error("Brevo API error:", err);
+      return;
+    }
 
     logger.info(`mail sent to ${to}`);
   } catch (error) {
